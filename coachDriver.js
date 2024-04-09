0cc450d42cbc9e40e618f5b1dc291008bc6daa4d
Tim <90668895+KrombopulosM16@users.noreply.github.com>
2024-04-08 21:55:47 -0400

    added basic interface

---
+++ coachDriver.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const GolfCoach_1 = require("./GolfCoach");
+let myGolfCoach = new GolfCoach_1.GolfCoach();
+console.log(myGolfCoach.getDailyWorkout());
---
+++ coachDriver.ts
@@ -0,0 +1,5 @@
+import { Coach } from './Coach';
+import { GolfCoach } from './GolfCoach';
+
+let myGolfCoach = new GolfCoach();
+console.log(myGolfCoach.getDailyWorkout());
